db8270c6667b19bb1cc58c6f2721fc111c59c8a7
Trung Nguyen <huynhanhtrung.nguyen@gmail.com>
2026-06-03 16:42:17 +0700

    fix: các api business của user không kiểm tra  business tồn tại

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 const auth = require('../middleware/auth');
-const adminOnly = require('../middleware/adminOnly');
+const businessAuth = require('../middleware/businessesAuth');
 
 router.get('/me', auth, userController.getCurrentUser);
 router.put('/me', auth, userController.updateUserProfile);
 router.delete('/me', auth, userController.deleteUserAccount);
 
 router.post('/businesses/register', auth, userController.registerBusiness);
-router.get('/businesses/me', auth, userController.getMyBusiness);
-router.put('/businesses/me', auth, userController.updateBusinessProfile);
-router.delete('/businesses/me', auth, userController.deleteBusinessAccount);
+router.get('/businesses/me', auth, businessAuth, userController.getMyBusiness);
+router.put('/businesses/me', auth, businessAuth, userController.updateBusinessProfile);
+router.delete('/businesses/me', auth, businessAuth, userController.deleteBusinessAccount);
 
 module.exports = router;
