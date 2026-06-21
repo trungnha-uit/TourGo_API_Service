a39c1d36484c5b7766b73a9065bc6342e75ece2a
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2026-06-21 20:10:36 +0700

    fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,68 @@
 const router = require('express').Router();
-const userController = require('../controllers/user.controller');
-const auth = require('../middleware/auth');
-const businessAuth = require('../middleware/businessesAuth');
 
-router.get('/me', auth, userController.getCurrentUser);
-router.put('/me', auth, userController.updateUserProfile);
-router.delete('/me', auth, userController.deleteUserAccount);
+const userController =
+require('../controllers/user.controller');
 
-router.post('/businesses/register', auth, userController.registerBusiness);
-router.get('/businesses/me', auth, businessAuth, userController.getMyBusiness);
-router.get('/businesses/me/listings', auth, businessAuth, userController.getMyListings);
-router.put('/businesses/me', auth, businessAuth, userController.updateBusinessProfile);
-router.delete('/businesses/me', auth, businessAuth, userController.deleteBusinessAccount);
+const auth =
+require('../middleware/auth');
+
+const businessAuth =
+require('../middleware/businessesAuth');
+
+const upload =
+require('../middleware/upload'); // thêm
+
+router.get(
+    '/me',
+    auth,
+    userController.getCurrentUser
+);
+
+router.put(
+    '/me',
+    auth,
+    upload.single('file'), // thêm
+    userController.updateUserProfile
+);
+
+router.delete(
+    '/me',
+    auth,
+    userController.deleteUserAccount
+);
+
+router.post(
+    '/businesses/register',
+    auth,
+    userController.registerBusiness
+);
+
+router.get(
+    '/businesses/me',
+    auth,
+    businessAuth,
+    userController.getMyBusiness
+);
+
+router.get(
+    '/businesses/me/listings',
+    auth,
+    businessAuth,
+    userController.getMyListings
+);
+
+router.put(
+    '/businesses/me',
+    auth,
+    businessAuth,
+    userController.updateBusinessProfile
+);
+
+router.delete(
+    '/businesses/me',
+    auth,
+    businessAuth,
+    userController.deleteBusinessAccount
+);
 
 module.exports = router;
